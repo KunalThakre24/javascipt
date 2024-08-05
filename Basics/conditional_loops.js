a42a3ec9ea6d0c2fb555d0b7b05ac7079cdc5225
Kunal Thakre <116478808+KunalThakre24@users.noreply.github.com>
2024-08-05 22:44:05 +0530

    Create conditional_loops.js

---
+++ Basics/conditional_loops.js
@@ -0,0 +1,36 @@
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+do-while loop
+let i = 0;
+do {
+    console.log("this is how do-while works");
+    i++;
+} while (i <= 5);
+
+terniary operator
+let i = 0;
+let ans = i == 0 ? 'yes' : 'no';
+console.log(ans);
+
+empty function
+let greeting;
+greetign = {};
+console.log(greetign);
+
+let a = 0;
+console.log(a++);
+
+var b=2;
+var b=3;
+console.log(b);
+
+console.log(typeof typeof 3);
+
+const num = [1,2,3];
+num[10] = 6;
+console.log(num);
+
+const a = "2";
+console.log(a());
